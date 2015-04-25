f6ef0415ddb4e2e802d2b99164f446db9d973394
Fatih Bozik <bozikfatih@gmail.com>
2015-04-25 18:25:01 +0300

    GameOverActivity ve SaveOverActivity AlertDialog olarak çıkacak şekilde değiştirildi.

--- app/src/main/java/com/example/fatih/hangman/GameActivity.java
+++ app/src/main/java/com/example/fatih/hangman/GameActivity.java
@@ -1,11 +1,11 @@
 package com.example.fatih.hangman;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
